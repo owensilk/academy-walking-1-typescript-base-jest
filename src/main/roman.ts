aa084c83b4002c659f9fbfb4e542f70fde72f93c
Owen Silk <owen.silk@cazoo.co.uk>
2021-02-23 15:19:47 +0000

    mob next [ci-skip]

--- src/main/roman.ts
+++ src/main/roman.ts
@@ -1,9 +1,8 @@
 export class Roman {
     convert(n: any): String {
-        if (n === 1) {
-            return "I"
-        } else if (n === 2) {
-            return "II"
+        let numerals = ["", "I", "II", "III"]
+        if (n) {
+            return numerals[n]
         }
         return ""
     }
--- src/test/roman.test.ts
+++ src/test/roman.test.ts
@@ -1,22 +1,15 @@
 import { Roman } from "../main/roman";
+const each = require("jest-each").default;
 
 describe('roman numeral converter', () => {
-    it("should return '' for null", () => {
+    each([
+        ["", null],
+        ["I", 1],
+        ["II", 2],
+        ["III", 3],
+        ["IIII", 4],
+    ]).it("should give %j for %j number", (roman_numeral: string, number: number) => {
         let calculator: Roman = new Roman();
-        expect(calculator.convert(null)).toBe("");
-    })
-
-    it("should return 'I' for 1", () => {
-        let calculator: Roman = new Roman();
-        expect(calculator.convert(1)).toBe("I");
-    })
-
-    it("should return 'II' for 2", () => {
-        let calculator: Roman = new Roman();
-        expect(calculator.convert(2)).toBe("II");
-    })
-    it("should return 'III' for 3", () => {
-        let calculator: Roman = new Roman();
-        expect(calculator.convert(3)).toBe("III");
-    })
+        expect(calculator.convert(number)).toBe(roman_numeral);
+    });
 })
