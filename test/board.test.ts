e7cee17fa61f64c4c1bee2b3daa05e95bed3f0d1
Owen Silk <owen.silk@cazoo.co.uk>
2021-03-02 15:43:14 +0000

    mob next [ci-skip]

--- test/board.test.ts
+++ test/board.test.ts
@@ -16,4 +16,11 @@ describe('Board test', () => {
         let board = Board();
         expect(() => board.set(1, 2, Play.O)).toThrow(new Error("Illegal move"))
     });
+
+    it("should make the multiple plays", () => {
+        let board = Board();
+        board.set(0, 0, Play.X)
+        board.set(0, 1, Play.O)
+        expect(board.state()).toEqual([["X", "O", ""], ["", "", ""], ["", "", ""]]);
+    });
 })
